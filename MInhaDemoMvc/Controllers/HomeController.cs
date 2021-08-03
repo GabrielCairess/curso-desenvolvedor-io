99e65a98e42c690f1b7267fa237dbfd6eeeb8703
Gabriel Caires <gbrlcaires@gmail.com>
2021-08-03 19:06:41 -0300

    commit inicial da demo

---
+++ MInhaDemoMvc/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MInhaDemoMvc.Models;
+using System.Diagnostics;
+
+namespace MInhaDemoMvc.Controllers
+{
+    [Route("")]
+    [Route("gestao-clientes")]
+    public class HomeController : Controller
+    {
+
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id?}")]
+        [Route("pagina-inicial/{id:int}/{categoria?}")]
+        public IActionResult Index(int id, string categoria)
+        {
+            return View();
+        }
+
+        [Route("sobre")]
+        [Route("sobre/{categoria:guid}")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Route("contact")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [Route("privacidade")]
+        [Route("politica-privacidade")]
+        public IActionResult Privacy()
+        {
+            var fileBytes = System.IO.File.ReadAllBytes(@"C:\Teste\arquivo.txt");
+            var fileName = "arquivo.txt";
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro encontrado")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MInhaDemoMvc/Models/Filme.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MInhaDemoMvc.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Título é obrigatório.")]
+        [StringLength(maximumLength: 60, MinimumLength = 3, ErrorMessage = "O Título precisa ter entre 3 e 60 caracteres.")]
+        [Display(Name = "Título")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo Data de Lançamento é obrigatório.")]
+        [DataType(DataType.DateTime, ErrorMessage = "Data inválida")]
+        [Display(Name = "Data de Lançamento")]
+        public DateTime DataLancamento { get; set; }
+
+        [Required(ErrorMessage = "O campo Gênero é obrigatório")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage = "Gênero em formato inválido.")]
+        [Display(Name = "Gênero")]
+        [StringLength(maximumLength: 30, MinimumLength = 3, ErrorMessage = "O Gênero deve ter entre 3 e 60 caracteres")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "O campo Valor é obrigatório")]
+        [Range(minimum: 1, maximum: 1000, ErrorMessage = "O valor deve ser de 1,00 a 1000,00")]
+        [Column(TypeName = "DECIMAL(18, 2)")]
+        public decimal Valor { get; set; }
+
+        [Required(ErrorMessage = "O campo avaiação é obrigatório.")]
+        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Somente números")]
+        [Display(Name = "Avaliação")]
+        public string Avaliacao { get; set; }
+    }
+}
