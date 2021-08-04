3617fb0febbfe983505f5037b472fec14a592976
Gabriel Caires <gbrlcaires@gmail.com>
2021-08-04 18:33:31 -0300

    adicionando exemplos de views e controllers

---
+++ MInhaDemoMvc/Data/MInhaDemoMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MInhaDemoMvc.Models;
+
+namespace MInhaDemoMvc.Data
+{
+    public class MInhaDemoMvcContext : DbContext
+    {
+        public MInhaDemoMvcContext (DbContextOptions<MInhaDemoMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MInhaDemoMvc.Models.Filme> Filme { get; set; }
+    }
+}
--- MInhaDemoMvc/Startup.cs
+++ MInhaDemoMvc/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MInhaDemoMvc.Data;
 
 namespace MInhaDemoMvc
 {
@@ -33,6 +35,9 @@ namespace MInhaDemoMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<MInhaDemoMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MInhaDemoMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
