6789e92f2cb689265581a9b228f76d07f616510c
Gabriel Caires <gbrlcaires@gmail.com>
2021-08-14 18:16:00 -0300

    aplicãção feita sem camadas, apenas com os scaffolds

---
+++ AppMvcBasica/Models/Enums/TipoFornecedor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcBasica.Models.Enums
+{
+    public enum TipoFornecedor
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
---
+++ AppMvcBasica/Models/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppMvcBasica.Models
+{
+    public class Produto : Entity
+    {
+        public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [StringLength(1000, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Imagem { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public decimal Valor { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        [DisplayName("Ativo ?")]
+        public bool Ativo { get; set; }
+
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
