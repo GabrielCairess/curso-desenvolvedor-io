f1057a164b866b67a5014b57e37ea8ff02a8ddb6
Gabriel Caires <gbrlcaires@gmail.com>
2021-09-15 19:13:43 -0300

    finalizando oajuste das views

---
+++ CadastroFornecedorProduto/src/DevIO.App/Extensions/MoedaAttributeAdapter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.App.Extensions
+{
+    public class MoedaAttributeAdapter : AttributeAdapterBase<MoedaAttribute>
+    {
+
+        public MoedaAttributeAdapter(MoedaAttribute moedaAttribute, IStringLocalizer stringLocalizer) : base(moedaAttribute, stringLocalizer)
+        {
+        }
+
+        public override void AddValidation(ClientModelValidationContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+
+            MergeAttribute(context.Attributes, key: "data-val", value: "true");
+            MergeAttribute(context.Attributes, key: "data-val-moeda", value: GetErrorMessage(context));
+            MergeAttribute(context.Attributes, key: "data-val-number", value: GetErrorMessage(context));
+        }
+
+        public override string GetErrorMessage(ModelValidationContextBase validationContext)
+        {
+            return "Moeda em formato inválido!";
+        }
+    }
+}
--- CadastroFornecedorProduto/src/DevIO.App/Startup.cs
+++ CadastroFornecedorProduto/src/DevIO.App/Startup.cs
@@ -1,15 +1,20 @@
 using DevIO.App.Data;
+using DevIO.App.Extensions;
 using DevIO.Business.Interfaces;
 using DevIO.Data.Context;
 using DevIO.Data.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace DevIO.App
 {
@@ -40,10 +45,28 @@ namespace DevIO.App
 
             services.AddAutoMapper(typeof(Startup));
 
+            services.AddMvc(o =>
+            {
+                o.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor((x, y) => "O valor '{0}' não é válido para {1}.");
+                o.ModelBindingMessageProvider.SetMissingBindRequiredValueAccessor(x => "Não foi fornecido um valor para o campo {0}.");
+                o.ModelBindingMessageProvider.SetMissingKeyOrValueAccessor(() => "Campo obrigatório.");
+                o.ModelBindingMessageProvider.SetMissingRequestBodyRequiredValueAccessor(() => "É necessário que o body na requisição não esteja vazio.");
+                o.ModelBindingMessageProvider.SetNonPropertyAttemptedValueIsInvalidAccessor((x) => "O valor '{0}' não é válido.");
+                o.ModelBindingMessageProvider.SetNonPropertyUnknownValueIsInvalidAccessor(() => "O valor fornecido é inválido.");
+                o.ModelBindingMessageProvider.SetNonPropertyValueMustBeANumberAccessor(() => "O campo deve ser um número.");
+                o.ModelBindingMessageProvider.SetUnknownValueIsInvalidAccessor((x) => "O valor fornecido é inválido para {0}.");
+                o.ModelBindingMessageProvider.SetValueIsInvalidAccessor((x) => "O valor fornecido é inválido para {0}.");
+                o.ModelBindingMessageProvider.SetValueMustBeANumberAccessor(x => "O campo deve ser um número.");
+                o.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(x => "O valor nulo é inválido.");
+
+                o.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            });
+
             services.AddScoped<MeuDbContext>();
             services.AddScoped<IProdutoRepository, ProdutoRepository>();
             services.AddScoped<IFornecedorRepository, FornecedorRepository>();
             services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddSingleton<IValidationAttributeAdapterProvider, MoedaValidationAttributeAdapterProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -67,6 +90,16 @@ namespace DevIO.App
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            var defaultCulture = new CultureInfo("pt-BR");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(defaultCulture),
+                SupportedCultures = new List<CultureInfo> { defaultCulture },
+                SupportedUICultures = new List<CultureInfo> { defaultCulture }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
 
             app.UseEndpoints(endpoints =>
             {
